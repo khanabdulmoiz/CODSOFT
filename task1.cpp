438d5083545a1f3427fd95cb9dade818fd3cfd49
Khan Abdul Moiz <khanabdulmoiz17@gmail.com>
2024-01-25 16:37:18 +0500

    Task-1

---
+++ task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int secretNumber = rand() % 100 + 1;
+    int userGuess;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!\n";
+    cout << "Try to guess the number between 1 and 100.\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> userGuess;
+        attempts++;
+
+        if (userGuess == secretNumber) {
+            cout << "Congratulations! You guessed the correct number in " << attempts << " attempts.\n";
+        } else if (userGuess < secretNumber) {
+            cout << "Too low! Try again.\n";
+        } else {
+            cout << "Too high! Try again.\n";
+        }
+
+    } while (userGuess != secretNumber);
+
+    return 0;
+}
